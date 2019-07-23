7dd94f701e859bbfb2dff6d0174fe8f481f73a55
jobizzness <jobizzness@gmail.com>
2019-07-23 17:17:10 +0800

    animating header

--- apps/remi/src/app/app.ts
+++ apps/remi/src/app/app.ts
@@ -55,7 +55,32 @@ export class App extends useLightDom {
     // Anything that's related to rendering should be done in here.
     return html`
       <!-- Header -->
-
+      <div class="bg">
+        <div class="overlay-primary"></div>
+        <div class="hero-bottom-wave">
+          <svg
+            class="white-wave"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 2185.67598 163.5116"
+            preserveAspectRatio="none"
+          >
+            <g fill="#FFF">
+              <path
+                id="wave-hero-a"
+                d="M0.00014879508281249998,169.299133541075 C0,121.45670655968749 0,73.61032456187499 0,25.77386223625 510.57,74.124400495 866.45,56.7873953784375 1102.43,29.9850213009375 1165.08,22.871175317812497 1217.35,15.314639165625 1305.37,8.8993063053125 1663.92,-17.2141530103125 1974.87,22.398967115625002 2185.67,61.323413463125 2185.67,97.31330516125 2185.67,133.303196859375 2185.67,169.2931688853125 1457.11,169.2931688853125 728.55,169.2931688853125 0,169.2931688853125 z"
+                data-original="M0,36.13176c510.57341-49.1484,866.45316-31.52357,1102.43632-4.28682,62.644,7.23026,114.91521,14.91582,202.936,21.4341C1663.92411,79.83119,1974.87411,39.56043,2185.676,0V163.5116H0Z"
+                data-svg-origin="0 0"
+                style="transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);"
+              ></path>
+              <path
+                id="wave-hero-b"
+                class="filler"
+                d="M.00015,169.346,0,25.69084c510.57343,49.14831,866.45322,31.52344,1102.43639,4.28665,62.644-7.23023,114.91523-14.91579,202.936-21.43411,358.55178-26.55218,669.50178,13.71854,880.30381,53.27894l.00014,107.5234Z"
+              ></path>
+            </g>
+          </svg>
+        </div>
+      </div>
       <app-header condenses reveals effects="waterfall">
         <mwc-linear-progress ?hidden=${!this.loading}></mwc-linear-progress>
         <app-toolbar class="toolbar-top">
